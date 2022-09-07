7dca075ef2fa48b5b85e8137c7800f0d34ef15c2
Vilasi <VilasiCoding@gmail.com>
2022-09-07 01:43:47 -0400

    Nav color change script added

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const NAV = document.getElementById('main-navbar');
+// console.log(NAV.getAttribute('class'));
+console.log(NAV.classList);
+
+// NAV.classList.add('scrolled');
+
 document.addEventListener('scroll', (e) => {
-  console.log(scrollY);
+  //   console.log(scrollY);
+  if (scrollY >= 100) {
+    console.log(scrollY);
+    NAV.classList.add('scrolled');
+  } else if (NAV) {
+    NAV.classList.remove('scrolled');
+  }
 });
